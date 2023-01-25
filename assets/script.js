e37a498812b4bde3491f117143ea2b58a48dd5c9
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2023-01-25 14:37:09 +0000

    functions to schedule inputs

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,31 @@
 //Daily Planner
 $(document).ready(function(){
-    var currentMoment = moment().format('dddd, MMMM, YYYY');
+    let currentMoment = moment().format('dddd, MMMM, YYYY');
     //Declared variable for the current    
     const WORKING_HOURS = ['9AM', '10AM', '11AM', '12PM','1PM','2PM','3PM','4PM','5PM']
     // Variables for the different time slots of working day
 
-    $('#currentDay').text(currentMoment); //
+    $('#currentDay').text(currentMoment); 
+
+
+    function getScheduledItemByHour(hour){
+        return localStorage.getItem(hour)
+    }
+
+    function storeScheduledItem(hour, scheduledItem){
+        localStorage.setItem(hour, scheduledItem)
+    }
+
+    function createTimeDataEl(hour){
+        var timeDataEl = $('<td>').addClass('hour').css({width: '5%'});
+        return timeDataEl.text(hour);
+    }
+    
+    function createTextDataEl(colorClass){
+        var textDataEl = $('<textarea col-10>').attr('style','width:80%');
+        return textDataEl.addClass(colorClass);
+    }
+    
 
 
 }) 
