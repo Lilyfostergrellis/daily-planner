6d9c2cbc1270cda0628da9eaf9519a6b144f8282
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2023-01-25 13:43:13 +0000

    declared variables for time slots in the day

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+//Daily Planner
+$(document).ready(function(){
+    var currentMoment = moment().format('dddd, MMMM Do');
+    //Declared variable for the current    
+    const WORKING_HOURS = ['9AM', '10AM', '11AM', '12PM','1PM','2PM','3PM','4PM','5PM']
+    // Variables for the different time slots of working day
+
+}) 
