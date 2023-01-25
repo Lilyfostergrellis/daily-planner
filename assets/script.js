922d8d6732d505fc04ada2e0045a472a22413c5f
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2023-01-25 13:49:55 +0000

    present day displayed on main page

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,11 @@
 //Daily Planner
 $(document).ready(function(){
-    var currentMoment = moment().format('dddd, MMMM Do');
+    var currentMoment = moment().format('dddd, MMMM, YYYY');
     //Declared variable for the current    
     const WORKING_HOURS = ['9AM', '10AM', '11AM', '12PM','1PM','2PM','3PM','4PM','5PM']
     // Variables for the different time slots of working day
+
+    $('#currentDay').text(currentMoment); //
+
 
 }) 
